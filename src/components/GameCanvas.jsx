c8a2b7c2a0040f536ce6ed9ae67aca84270fdf2d
sprotte <metatron1972@live.de>
2022-06-27 16:30:49 +0200

    sprottefertig

--- src/components/GameCanvas.jsx
+++ src/components/GameCanvas.jsx
@@ -54,7 +54,7 @@ export const GameCanvas = () => {
           gameControl.current.startGame();
         }}
       >
-        Start
+        New Game
       </button>
       <p className="punkte">Score: {score}</p> {/* Angezeigter Punktezähler */}
       {/* Muss noch eine Routine Geschrieben werden. */}
@@ -62,3 +62,47 @@ export const GameCanvas = () => {
     </>
   );
 };
+/* ---------------------Aniemierter Hintergund--------------------------------- */
+// verschiedene Farb Punkte
+const colors = ["#E0FFFF", "#D2B48C", "#BEBEBE", "#FF4040", "#F5F5DC"];
+
+const numBalls = 50;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12,
+  };
+
+  // eslint-disable-next-line no-unused-vars
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` },
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out",
+    }
+  );
+});
+/* --------------------------------------------------------------------------- */
