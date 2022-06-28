56478732b59928538d7c6a501291fee78a467c42
sprotte <metatron1972@live.de>
2022-06-28 14:42:29 +0200

    sprottefertig

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
             autoPlay
             controls
             loop
-            volume={0.0}
+            volume={0.4}
           />
           <GameCanvas />
           <Footer />
